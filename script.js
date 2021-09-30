8638411c7965cbd1f48df8a58e2831b9ebb44747
hossain377 <hossainahmed377@gmail.com>
2021-09-30 11:13:36 +0600

    countingNumber

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
+{/* <div class="counter-container">
+    <i class="fas fa-users fa-3X"></i>
+    <div class="counter" data-target="1500"></div>
+    <span>our happy client</span>
+   </div> */}
 
-const calculateTemp = () =>{
-    const numberTemp = document.getElementById('temp').value;
-    //console.log(numberTemp);
-    const tempSelected = document.getElementById('temp_diff');
-    const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
-
-    const celToFah=(cel) =>{
-        let fahrenheit = Math.round((cel * 9/5) + 32);
-        return fahrenheit ;
-    }
-    const fahToCel = (fah) =>{
-        let celcius = Math.round((fah - 32) * 5/9);
-        return celcius ;
-    }
-
-    let result;
-    if(valueTemp == 'cel'){
-        result = celToFah(numberTemp);
-        document.getElementById('resultContainer').innerHTML = `= ${result} fahrenheit`;
-
-    }else{
-        result = fahToCel(numberTemp);
-        document.getElementById('resultContainer').innerHTML = `= ${result} celcius`;
+const counters = document.querySelectorAll('.counter');
+counters.forEach((counter) =>{
+    counter.innerHTML= 0;
+    const updateCounter = () =>{
+        const targetCount = +counter.getAttribute('data-target');
+        //console.log(typeof targetCount);
+        const startingCount = Number(counter.innerHTML);
+        const incr = targetCount/100;
+        if(startingCount< targetCount){
+            counter.innerHTML = `${Math.round(startingCount + incr)}`;
+            setTimeout(updateCounter, 100);
+        }else{
+            counter.innerHTML = targetCount; 
+        }
 
     }
-
-}
+    updateCounter();
+})
