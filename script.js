faa1a66e0de74581f551d457b7858b449b3c3f0b
hossain377 <hossainahmed377@gmail.com>
2021-10-01 12:51:10 +0600

    final project thapa

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
-{/* <div class="counter-container">
-    <i class="fas fa-users fa-3X"></i>
-    <div class="counter" data-target="1500"></div>
-    <span>our happy client</span>
-   </div> */}
+const addButton = document.querySelector('#add');
 
-const counters = document.querySelectorAll('.counter');
-counters.forEach((counter) =>{
-    counter.innerHTML= 0;
-    const updateCounter = () =>{
-        const targetCount = +counter.getAttribute('data-target');
-        //console.log(typeof targetCount);
-        const startingCount = Number(counter.innerHTML);
-        const incr = targetCount/100;
-        if(startingCount< targetCount){
-            counter.innerHTML = `${Math.round(startingCount + incr)}`;
-            setTimeout(updateCounter, 100);
-        }else{
-            counter.innerHTML = targetCount; 
-        }
+ const updateLSData= () =>{
+     const textAreaData = document.querySelectorAll('textarea');
+     const notes = [];
+     console.log(textAreaData);
+     textAreaData.forEach((note) => {
+         return notes.push(note.value);
+     });
+     console.log(notes);
+     localStorage.setItem('notes', JSON.stringify(notes));
+ }
 
-    }
-    updateCounter();
-})
+const addNewNote = (text = '') =>{
+    const note= document.createElement('div');
+     note.classList.add('note');
+     const htmlData = `
+     
+        <div class="operation">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main ${text ? "" : " hidden"}"></div>
+        <textarea class="${text ? "hidden" : "" }" ></textarea> `;
+
+     note.insertAdjacentHTML('afterbegin', htmlData);
+     //console.log(note);
+
+     //getting the reference
+     const editButton = note.querySelector('.edit');
+     const delButton = note.querySelector('.delete');
+     const mainDiv = note.querySelector('.main');
+     const textArea = note.querySelector('textarea');
+
+    //deleting the node
+    delButton.addEventListener('click',() =>{
+        note.remove();
+        updateLSData();
+    } )
+
+    //toggle using edit button
+    textArea.value= text;
+    mainDiv.innerHTML = text;
+
+    textArea.addEventListener('change', (event) =>{
+        const value = event.target.value;
+        mainDiv.innerHTML = value;
+        console.log(value);
+    })
+
+
+    editButton.addEventListener('click', () => {
+        mainDiv.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+
+        updateLSData();
+    })
+
+
+
+     document.body.appendChild(note);
+}
+
+//getting data from localstorage
+const notes = JSON.parse(localStorage.getItem('notes'));
+if(notes){notes.forEach((note) => addNewNote(note))}
+
+addButton.addEventListener('click', () => addNewNote() );
